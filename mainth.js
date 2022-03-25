b0f0f2d7bb8910f45780f13923ff219eaecd67ed
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2022-03-25 09:54:54 +0300

    mohamad login

---
+++ mainth.js
@@ -0,0 +1,42 @@
+let username=document.querySelector(".username")
+let container=document.querySelector(".login")
+let form =document.querySelector("form")
+let btn=document.querySelector(".btn")
+let table=document.querySelector(".m388")
+let table403=document.querySelector(".m403")
+let password=document.querySelector(".password")
+/*btn.onclick=function(){
+  if(username.value==''){
+container.innerText+=("is empty")
+  }else if(username.value==388 ){
+   
+    table.classList.add("unhiden")
+
+  }
+  if(username.value==''){
+    container.innerText+=("is empty")
+      }else if(username.value==403 && password.value==1998){
+        table.remove();
+    table403.classList.add("unhiden4")
+    
+      }
+}
+*/
+btn.addEventListener("click",(eo) => {
+  eo.preventDefault();
+  if(username.value==''){
+    container.innerText+=("is empty")
+      }else if(username.value==388 ){
+       
+        table.classList.add("unhiden")
+    
+      }
+      if(username.value==''){
+        container.innerText+=("is empty")
+          }else if(username.value==403 && password.value==1998){
+            table.remove();
+        table403.classList.add("unhiden4")
+        
+          }
+    
+})
