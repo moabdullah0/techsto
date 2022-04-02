2242ea1c2b24e2301c4e9529bf37774446cb363c
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2022-04-02 13:54:07 +0300

    mocom

---
+++ main.js
@@ -0,0 +1,13 @@
+ScrollReveal({ 
+    reset: true ,
+    distance:'60px',
+    duration:2500,
+    delay:400
+
+});
+ScrollReveal().reveal('.ss33,.ss22,.ss11', { delay: 500 ,origin:'left' });
+ScrollReveal().reveal('.imgst1,.imgst2,.imgst3', { delay: 600 ,origin:'bottom' });
+ScrollReveal().reveal('.btn11,.btn22,.btn33', { delay: 600 ,origin:'top' });
+ScrollReveal().reveal('.fotimg', { delay: 600 ,origin:'right' });
+ScrollReveal().reveal('.fottext', { delay: 600 ,origin:'right' });
+ScrollReveal().reveal('.fottext1', { delay: 400 ,origin:'top' });
--- mainth.js
+++ mainth.js
@@ -4,24 +4,9 @@ let form =document.querySelector("form")
 let btn=document.querySelector(".btn")
 let table=document.querySelector(".m388")
 let table403=document.querySelector(".m403")
+let table420=document.querySelector(".m420")
 let password=document.querySelector(".password")
-/*btn.onclick=function(){
-  if(username.value==''){
-container.innerText+=("is empty")
-  }else if(username.value==388 ){
-   
-    table.classList.add("unhiden")
 
-  }
-  if(username.value==''){
-    container.innerText+=("is empty")
-      }else if(username.value==403 && password.value==1998){
-        table.remove();
-    table403.classList.add("unhiden4")
-    
-      }
-}
-*/
 btn.addEventListener("click",(eo) => {
   eo.preventDefault();
   if(username.value==''){
@@ -38,5 +23,9 @@ btn.addEventListener("click",(eo) => {
         table403.classList.add("unhiden4")
         
           }
-    
+          if(username.value==''){
+            container.innerText+=("is empty")
+              }else if(username.value==420 ){
+                table403.remove();
+            table420.classList.add("unhiden420")}
 })
