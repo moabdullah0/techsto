aa96f279bd7a4e2e47f0be048315e9bedcf1a5d2
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2022-04-08 11:33:48 +0300

    mmfirst

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ ScrollReveal({
     reset: true ,
     distance:'60px',
     duration:2500,
-    delay:400
+    delay:100
 
 });
-ScrollReveal().reveal('.ss33,.ss22,.ss11', { delay: 500 ,origin:'left' });
-ScrollReveal().reveal('.imgst1,.imgst2,.imgst3', { delay: 600 ,origin:'bottom' });
-ScrollReveal().reveal('.btn11,.btn22,.btn33', { delay: 600 ,origin:'top' });
-ScrollReveal().reveal('.fotimg', { delay: 600 ,origin:'right' });
+ScrollReveal().reveal('.ss33,.ss22,.ss11', { delay: 100 ,origin:'left' });
+ScrollReveal().reveal('.imgst1,.imgst2,.imgst3', { delay: 100 ,origin:'bottom' });
+ScrollReveal().reveal('.btn11,.btn22,.btn33', { delay: 100 ,origin:'top' });
+ScrollReveal().reveal('.fotimg', { delay: 100 ,origin:'right' });
 ScrollReveal().reveal('.fottext', { delay: 600 ,origin:'right' });
 ScrollReveal().reveal('.fottext1', { delay: 400 ,origin:'top' });
+
