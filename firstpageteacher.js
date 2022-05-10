9eae80f15a08b0ff9bc2d2f0800f08e7a85fe52d
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2022-05-10 18:48:19 +0300

    sdjka

---
+++ firstpageteacher.js
@@ -0,0 +1,18 @@
+let username=document.querySelector(".username")
+let password=document.querySelector(".password")
+let btn =document.querySelector(".btn1")
+error1=document.querySelector(".erorrus")
+error2=document.querySelector(".erorrps")
+btn.addEventListener("click",(eo) => {
+    eo.preventDefault();
+    if(username.value==''||username.value!=388 ){
+    error1.classList.add("erorrusunh")
+}
+else if(password.value==''||password.value!=1998){
+error2.classList.add("erorrpsunh")
+}
+       else if(username.value==388 && password.value==1998 ){
+         
+          window.location="teacher.html"
+      
+        }})
--- s1.js
+++ s1.js
@@ -5,7 +5,7 @@ ScrollReveal({
     delay:150
 
 });
-ScrollReveal().reveal('.ss33,.ss22,.ss11', { delay: 150 ,origin:'left' });
+
 ScrollReveal().reveal('.imgst1,.imgst2,.imgst3', { delay: 150 ,origin:'bottom' });
 ScrollReveal().reveal('.btn11,.btn22,.btn33', { delay: 150 ,origin:'top' });
 ScrollReveal().reveal('.fotimg', { delay: 200 ,origin:'right' });
